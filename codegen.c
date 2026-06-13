f30578573e3563a99e6c38b4620f6e98c294df06
Kotaro Noguchi <kotaronoguchi61@gmail.com>
2026-06-13 14:49:11 +0900

    expression statement

--- codegen.c
+++ codegen.c
@@ -7,6 +7,10 @@ void gen(Node *node) {
         case ND_NUM:
             printf("  push %d\n", node->val);
             return;
+        case ND_EXPR_STMT: // push but cancel(?) it
+            gen(node->lhs);
+            printf("  add rsp, 8\n");
+            return;
         case ND_RETURN:
             gen(node->lhs);
             printf("  pop rax\n");
@@ -66,7 +70,6 @@ void codegen(Node *node) {
 
     for (Node *n = node; n; n = n->next) {
         gen(n);
-        printf(" pop rax\n");
     }
 
     printf("  ret\n");
--- common.h
+++ common.h
@@ -28,6 +28,7 @@ typedef enum {
     ND_LT,
     ND_LE,
     ND_RETURN,
+    ND_EXPR_STMT,
     ND_NUM,
 } NodeKind;
 
